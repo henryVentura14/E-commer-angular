bd0494d433f90c851bcf5660588288bbbf0ee808
henryVentura14 <henry.ventura14@gmail.com>
2021-03-03 11:34:31 -0500

    add total products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { SpinnerComponent } from './components/spinner/spinner.component';
 import { FormComponent } from './components/form/form.component';
 import { BudgetComponent } from './components/budget/budget.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SpinnerComponent,
     FormComponent,
     BudgetComponent,
   ],
--- src/app/components/budget/budget.component.ts
+++ src/app/components/budget/budget.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProductService } from '../../service/product.service';
 import { Product } from '../../interfaces/products.interface';
 
+
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -9,6 +10,8 @@ import { Product } from '../../interfaces/products.interface';
   encapsulation: ViewEncapsulation.None
 })
 export class BudgetComponent implements OnInit {
+  public total:any;
+  public list:any;
 
   constructor(public productService: ProductService) {
   }
@@ -17,10 +20,20 @@ export class BudgetComponent implements OnInit {
     this.getProducts();
   }
 
+  optionCurrency = { style: 'currency', currency: 'USD' };
+  formatCurrency = new Intl.NumberFormat('en-US', this.optionCurrency);
+
+
   getProducts() {
     this.productService.getProducts()
       .subscribe(res => {
         this.productService.product = res as Product[];
+        this.list = this.productService.product .map(item => ({
+          price: Math.floor(item.price)
+        }));
+         this.total = this.list.reduce((prev, next) => prev + next.price, 0);
+         console.log(this.formatCurrency.format(this.total))
+
       })
   }
 }
